77aff3397923afcdfe17be0d902edb5c4c20c2ef
Bruno Augusto <bruno.garcia@jussi.com.br>
2022-03-18 13:48:17 -0300

    css and order by improvements

--- client/brunoaugusto/js/readData.js
+++ client/brunoaugusto/js/readData.js
@@ -1,8 +1,9 @@
 import listaNoticias from "./api.js";
 
-async function render(data){
+export default async function render(data){
     let render = await Promise.all(data.map((noticia)=>{
         const ul = document.querySelector('.lista__container')
+        
         const li = document.createElement('li')
         const img = document.createElement('img')
         const divElementos = document.createElement('div')
@@ -12,7 +13,7 @@ async function render(data){
         li.classList.add('lista__container__noticia')
         img.src = noticia.image
         img.classList.add('lista__container__noticia__imagem')
-        
+        img.onerror = ()=>{img.src= `https://static.escolakids.uol.com.br/image/a-noticia-kids.Ijpg.jpg`}
         divElementos.classList.add('lista__container__noticia__elementos')
         divElementos.innerHTML = `<div class="lista__container__noticia__elementos--titulo"><a href="${noticia.url}">
         <h2 class="lista__container__noticia__titulo">${noticia.title}</h2></a>
@@ -23,14 +24,28 @@ async function render(data){
         <p>${noticia.excerpt}</p>`
 
         divCategoria.classList.add('lista__container__noticia__elementos--categoria')
-        noticia.categories.forEach(categoria => {
+        if(noticia.categories.length === 0){
             const spanCategoria = document.createElement('span')
             const icon = document.createElement('i')
             icon.className = 'fa-solid fa-tag'
-            spanCategoria.innerHTML = categoria.name
+            spanCategoria.innerHTML = "Sem categoria"
             divCategoria.appendChild(icon)
             divCategoria.appendChild(spanCategoria)
-        });
+        }else{
+            noticia.categories.forEach(categoria => {
+                const spanCategoria = document.createElement('span')
+                const icon = document.createElement('i')
+                icon.className = 'fa-solid fa-tag'
+                
+                    spanCategoria.innerHTML = categoria.name
+                
+                
+                divCategoria.appendChild(icon)
+                divCategoria.appendChild(spanCategoria)
+            });
+        }
+
+        
         
         divElementos.appendChild(divCategoria)
         li.appendChild(img)
@@ -41,4 +56,4 @@ async function render(data){
     })) 
 }
 
-render(await listaNoticias())
+window.addEventListener("load", render(await listaNoticias()))
