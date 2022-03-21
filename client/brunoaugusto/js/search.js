45c0e0c0e78e5514c82265b1a96d70f47aedf164
Bruno Augusto <bruno.garcia@jussi.com.br>
2022-03-21 14:35:12 -0300

    Creating search function

---
+++ client/brunoaugusto/js/search.js
@@ -0,0 +1,30 @@
+import listaNoticias from "./api.js";
+import render from "./readData.js";
+
+const searchInput = document.querySelector('[data-search]')
+searchInput.addEventListener("input", search)
+async function search(){
+    const cache = JSON.parse(localStorage.getItem("data"))
+    let palavra = searchInput.value.toLowerCase() 
+    let arrayPesquisado = cache.map((element)=>{
+       let titulo = element.title.toLowerCase()
+        if(titulo.includes(palavra)){
+            return element
+        }
+    })
+    let arrayFiltrado = arrayPesquisado.filter((element)=>{
+        if(element != undefined){
+            return element
+        }
+    })
+    const ul = document.querySelector('.lista__container')
+    ul.innerHTML =''
+    if(arrayFiltrado.length != 0){
+        render(arrayFiltrado)
+    }else{
+        ul.innerHTML = '<li class="lista__container__noticia--erro">Nada encontrado</li>'
+        console.log('oi')
+    }
+    
+    
+}
