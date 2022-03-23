44beb1fd83980fb3308cbce744ac6a53428baf45
Bruno Augusto <bruno.garcia@jussi.com.br>
2022-03-23 16:41:27 -0300

    Favorite function

---
+++ client/brunoaugusto/js/favorite.js
@@ -0,0 +1,26 @@
+let favoritos = []
+export default function favorite(event){
+    event.target.classList.toggle('fa-solid')
+    event.target.classList.toggle('fa-regular')
+    
+    if(event.target.classList.contains('fa-solid') && favoritos.includes(event.target.parentElement.innerText) != true){
+        const tituloFavorito = event.target.parentElement.innerText
+        favoritos.push(tituloFavorito)
+        localStorage.setItem('Favoritos',JSON.stringify(favoritos))
+    }else if(localStorage.getItem('Favoritos').includes(event.target.parentElement.innerText)){
+         const tituloFavorito = event.target.parentElement.innerText
+         favoritos = JSON.parse(localStorage.getItem('Favoritos'))
+        let favoritosFiltrado = favoritos.filter((element)=>{
+            return element != tituloFavorito
+        })
+        favoritos = favoritosFiltrado
+        localStorage.setItem('Favoritos',JSON.stringify(favoritos)) 
+        console.log('oi')
+        
+    }
+    
+    
+}
+
+
+
--- client/brunoaugusto/js/readData.js
+++ client/brunoaugusto/js/readData.js
@@ -1,4 +1,4 @@
-import listaNoticias from "./api.js";
+import favorite from "./favorite.js"
 
 export default async function render(data){
     const ul = document.querySelector('.lista__container')
@@ -19,8 +19,7 @@ export default async function render(data){
         divElementos.classList.add('lista__container__noticia__elementos')
         divElementos.innerHTML = `<div class="lista__container__noticia__elementos--titulo"><a href="${noticia.url}">
         <h2 class="lista__container__noticia__titulo">${noticia.title}</h2></a>
-        <i class="fa-regular fa-heart"></i>
-        <i class="fa-solid fa-heart hide"></i>
+        <i class="fa-regular fa-heart" data-heartregular></i>
         </div>
         <span>${noticia.date_published}</span>
         <p>${noticia.excerpt}</p>`
@@ -56,6 +55,16 @@ export default async function render(data){
         ul.appendChild(li)
 
     })) 
+
+    const coracao = document.querySelectorAll('[data-heartregular]')
+    coracao.forEach((element)=>{
+        if(localStorage.getItem('Favoritos') != null && localStorage.getItem('Favoritos').includes(element.parentElement.innerText)){
+            element.classList.toggle('fa-solid')
+            element.classList.toggle('fa-regular')
+        }
+    element.addEventListener('click',favorite)
+    
+   })
 }
 
 //window.addEventListener("load", render(await listaNoticias()))
